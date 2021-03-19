c339f99fa0ae696af8fa8f6f2ab7aafb50bf8ba4
marianomat <marianopereyra95@gmail.com>
2021-03-19 17:45:03 -0300

    Seccion 7
    Route refactoring, adding stars rating to reviews

--- routes/users.js
+++ routes/users.js
@@ -1,56 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
 const catchAsync = require("../utils/catchAsync");
 const passport = require("passport");
+const users = require("../controllers/users");
 
-router.get("/register", (req, res) => {
-	res.render("users/register");
-});
+router
+	.route("/register")
+	.get(users.renderRegister)
+	.post(catchAsync(users.register));
 
-router.post(
-	"/register",
-	catchAsync(async (req, res, next) => {
-		try {
-			const { email, username, password } = req.body;
-			const newUser = new User({ email, username });
-			const registeredUser = await User.register(newUser, password);
-			//*Log the user in
-			req.login(registeredUser, (err) => {
-				if (err) {
-					return next(err);
-				}
-				req.flash("success", "Welcome to YelpCamp");
-				res.redirect("/campgrounds");
-			});
-		} catch (e) {
-			req.flash("error", e.message);
-			res.redirect("/register");
-		}
-	})
-);
+router
+	.route("/login")
+	.get(users.renderLogin)
+	.post(
+		passport.authenticate("local", {
+			failureFlash: true,
+			failureRedirect: "/login",
+		}),
+		users.login
+	);
 
-router.get("/login", (req, res) => {
-	res.render("users/login");
-});
-router.post(
-	"/login",
-	passport.authenticate("local", {
-		failureFlash: true,
-		failureRedirect: "/login",
-	}),
-	(req, res) => {
-		req.flash("success", "Welcome Back!");
+router.get("/logout", users.logout);
 
-		//*Checkeo si viene de otra pagina
-		const redirectUrl = req.session.returnTo || "/campgrounds";
-		delete req.session.returnTo;
-		res.redirect(redirectUrl);
-	}
-);
-router.get("/logout", (req, res) => {
-	req.logout();
-	req.flash("success", "Logged out!");
-	res.redirect("/campgrounds");
-});
 module.exports = router;
