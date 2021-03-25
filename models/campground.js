d58401df49d506d110bb803d6d5427fa3a969813
marianomat <marianopereyra95@gmail.com>
2021-03-25 17:19:30 -0300

    Added cluster maps
    Added cluster maps

--- models/campground.js
+++ models/campground.js
@@ -8,33 +8,47 @@ imageSchema.virtual("thumbnail").get(function () {
 	console.log(this);
 	return this.url.replace("/upload", "/upload/w_200");
 });
-const CampgroundSchema = new Schema({
-	title: String,
-	price: Number,
-	description: String,
-	location: String,
-	geometry: {
-		type: {
-			type: String,
-			enum: ["Point"],
-			required: true,
+
+const options = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new Schema(
+	{
+		title: String,
+		price: Number,
+		description: String,
+		location: String,
+		geometry: {
+			type: {
+				type: String,
+				enum: ["Point"],
+				required: true,
+			},
+			coordinates: {
+				type: [Number],
+				required: true,
+			},
 		},
-		coordinates: {
-			type: [Number],
-			required: true,
-		},
-	},
-	images: [imageSchema],
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: "User",
-	},
-	reviews: [
-		{
+		images: [imageSchema],
+		author: {
 			type: Schema.Types.ObjectId,
-			ref: "Review",
+			ref: "User",
 		},
-	],
+		reviews: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Review",
+			},
+		],
+	},
+	options //*tHIS IS TO PASS VIRTUALS IN
+);
+
+//*For the popup mapbox
+CampgroundSchema.virtual("properties.popUpMarkup").get(function () {
+	return `<strong><a href="/campgrounds/${
+		this._id
+	}">${this.title}</a></strong>
+	<p>${this.description.substring(0, 20)}...</p>`;
 });
 
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
